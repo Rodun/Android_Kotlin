69fdce0df6b9a7ee67185a6b6bd920161fc73270
Rodun <yo_op@naver.com>
2020-03-08 15:29:19 +0900

    variable 업로드

---
+++ LottoManager/app/src/main/java/com/alpharodun/lottomanager/Kotlin/Variable_01.kt
@@ -0,0 +1,10 @@
+package com.alpharodun.lottomanager.Kotlin
+
+var Chicken = 23000
+val leg = 2
+
+fun main (args:Array<String>){
+    println(Chicken * 2)
+    println("출력 입니다.")
+    println(leg)
+}
